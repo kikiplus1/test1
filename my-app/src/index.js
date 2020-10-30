c06a853df6382cc6b1f6d4015cf35adfcd431f32
kikiplus1@naver.com <kikiplus1@naver.com>
2020-10-30 15:54:45 +0900

    'test'

---
+++ my-app/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Signup from "./Signup";
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+      <Signup />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
